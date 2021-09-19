afda530981f644d59e5baf5b54086c3f1c41f259
code750 <gitdmlwls@gmail.com>
2021-09-19 23:04:33 +0900

    my app 2 - in variable

--- src/App.java
+++ src/App.java
@@ -1,12 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Value of supply : "+10000.0);
-        System.out.println("VAT : "+(10000.0*0.1) );
-        System.out.println("Total : "+(10000.0 + 10000.0 * 0.1) );
-        System.out.println("Expense : "+(10000.0 * 0.3) );
-        System.out.println("Income : "+(10000.0 - 10000.0 * 0.3) );
-        System.out.println("Dividend 1 : "+(10000.0 - 10000.0 * 0.3) * 0.5 );
-        System.out.println("Dividend 2 : "+(10000.0 - 10000.0 * 0.3) * 0.3 );
-        System.out.println("Dividend 3 : "+(10000.0 - 10000.0 * 0.3) * 0.2 );
+        double valueOfSupply = 10000.0;
+        double vatRate = 0.1;
+        double expenceRate = 0.3;
+        double Vat = valueOfSupply * vatRate;
+        double Total = valueOfSupply + valueOfSupply * vatRate;
+        double Income = valueOfSupply - valueOfSupply * expenceRate;
+        double Dividend = valueOfSupply - valueOfSupply * 0.3;
+
+        System.out.println("Value of supply : "+valueOfSupply);
+        System.out.println("VAT : "+Vat );
+        System.out.println("Total : "+Total );
+        System.out.println("Expense : "+(valueOfSupply * expenceRate) );
+        System.out.println("Income : "+Income );
+        System.out.println("Dividend 1 : "+Dividend  * 0.5);
+        System.out.println("Dividend 2 : "+Dividend * 0.3 );
+        System.out.println("Dividend 3 : "+Dividend * 0.2 );
     }
 }
