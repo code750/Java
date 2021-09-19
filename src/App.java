5e342e193ec5fded87f5d7fcb1a168264c48e8cc
code750 <gitdmlwls@gmail.com>
2021-09-19 23:24:05 +0900

    my app - input

--- src/App.java
+++ src/App.java
@@ -1,17 +1,20 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        double valueOfSupply = 10000.0;
+        String valueOfSupply = JOptionPane.showInputDialog("Enter a Supply : ");
+        Double Supply = Double.parseDouble(valueOfSupply);
         double vatRate = 0.1;
         double expenceRate = 0.3;
-        double Vat = valueOfSupply * vatRate;
-        double Total = valueOfSupply + valueOfSupply * vatRate;
-        double Income = valueOfSupply - valueOfSupply * expenceRate;
-        double Dividend = valueOfSupply - valueOfSupply * 0.3;
+        double Vat = Supply * vatRate;
+        double Total = Supply + Supply * vatRate;
+        double Income = Supply - Supply * expenceRate;
+        double Dividend = Supply - Supply * 0.3;
 
         System.out.println("Value of supply : "+valueOfSupply);
         System.out.println("VAT : "+Vat );
         System.out.println("Total : "+Total );
-        System.out.println("Expense : "+(valueOfSupply * expenceRate) );
+        System.out.println("Expense : "+(Supply * expenceRate) );
         System.out.println("Income : "+Income );
         System.out.println("Dividend 1 : "+Dividend  * 0.5);
         System.out.println("Dividend 2 : "+Dividend * 0.3 );
