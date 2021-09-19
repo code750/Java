d8e81c0397734f0da380266457937790a96fe233
code750 <gitdmlwls@gmail.com>
2021-09-19 23:42:53 +0900

    my app -use  if

--- src/App.java
+++ src/App.java
@@ -7,17 +7,30 @@ public class App {
         double vatRate = 0.1;
         double expenceRate = 0.3;
         double Vat = Supply * vatRate;
-        double Total = Supply + Supply * vatRate;
-        double Income = Supply - Supply * expenceRate;
-        double Dividend = Supply - Supply * 0.3;
+        double Total = Supply + Vat;
+        double expence = Supply * expenceRate;
+        double Income = Supply - expence;
+
+        double Dividend1;
+        double Dividend2;
+        double Dividend3;
 
         System.out.println("Value of supply : "+valueOfSupply);
         System.out.println("VAT : "+Vat );
         System.out.println("Total : "+Total );
         System.out.println("Expense : "+(Supply * expenceRate) );
         System.out.println("Income : "+Income );
-        System.out.println("Dividend 1 : "+Dividend  * 0.5);
-        System.out.println("Dividend 2 : "+Dividend * 0.3 );
-        System.out.println("Dividend 3 : "+Dividend * 0.2 );
+        if (Income < 10000.0){
+            Dividend1 = Income * 0.5;
+            Dividend2 = Income * 0.0;
+            Dividend3 = Income * 0.0;
+        } else {
+            Dividend1 = Income * 0.5;
+            Dividend2 = Income * 0.3;
+            Dividend3 = Income * 0.2;
+        }
+        System.out.println("Dividend1 : "+Dividend1);
+        System.out.println("Dividend2 : "+Dividend2);
+        System.out.println("Dividend3 : "+Dividend3);
     }
 }
