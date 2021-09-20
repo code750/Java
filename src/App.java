43c9580b45da1dc6e08f7c880266d378d1f845f9
code750 <gitdmlwls@gmail.com>
2021-09-20 21:59:37 +0900

    my app - method

--- src/App.java
+++ src/App.java
@@ -1,30 +1,55 @@
 import javax.swing.JOptionPane;
 
 public class App {
+    public static double vatRate;
+    public static String valueOfSupply = JOptionPane.showInputDialog("Enter a Supply : ");
+    public static double Supply;
+    public static double expenceRate;
+    public static double Income;
+    public static double Dividend1;
+    public static double Dividend2;
+    public static double Dividend3;
+    public static double Vat;
+    public static double Total;
+    
     public static void main(String[] args) throws Exception {
-        String valueOfSupply = JOptionPane.showInputDialog("Enter a Supply : ");
-        Double Supply = Double.parseDouble(valueOfSupply);
-        double vatRate = 0.1;
-        double expenceRate = 0.3;
-        double Vat = Supply * vatRate;
-        double Total = Supply + Vat;
-        double expence = Supply * expenceRate;
-        double Income = Supply - expence;
+        vatRate = 0.1;
+        
+        Supply =  Double.parseDouble(valueOfSupply);
+        expenceRate = 0.3;
+        Vat = getVAT();
+        Total = Total();
+        Income = getIncome();
         
         double[] dividendRates = new double[3];
         dividendRates[0] = 0.5;
         dividendRates[1] = 0.3;
         dividendRates[2] = 0.2;
 
-        double Dividend1;
-        double Dividend2;
-        double Dividend3;
+        prints();
+        
+        getDividend(Income, dividendRates);
 
+        int i = 0;
+        while(i < dividendRates.length) {
+            System.out.println("Dividend1 : "+Income * dividendRates[i]);
+            i= i+1;
+        }
+    }
+
+    private static void prints() {
         System.out.println("Value of supply : "+valueOfSupply);
-        System.out.println("VAT : "+Vat );
+        System.out.println("VAT : "+ Vat );
         System.out.println("Total : "+Total );
-        System.out.println("Expense : "+(Supply * expenceRate) );
+        System.out.println("Expense : " +Expence() );
         System.out.println("Income : "+Income );
+    }
+
+    private static double getIncome() {
+        return Supply - Expence();
+    }
+
+    private static void getDividend(double Income, double[] dividendRates) {
         if (Income < 10000.0){
             Dividend1 = Income * dividendRates[0];
             Dividend2 = Income * 0.0;
@@ -34,10 +59,17 @@ public class App {
             Dividend2 = Income * dividendRates[1];
             Dividend3 = Income * dividendRates[2];
         }
-        int i = 0;
-        while(i < dividendRates.length) {
-            System.out.println("Dividend1 : "+Income * dividendRates[i]);
-            i= i+1;
-        }
+    }
+
+    private static double Expence() {
+        return Supply * expenceRate;
+    }
+
+    private static double Total() {
+        return Supply + getVAT();
+    }
+
+    private static double getVAT() {
+        return Supply * vatRate;
     }
 }
